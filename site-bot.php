5eee077a04fdfd21df11bec0d3268f90764fc716
Piyasopon <nottee.p@hotmail.com>
2019-07-01 11:27:17 +0700

    first sitebot

--- site-bot.php
+++ site-bot.php
@@ -1,83 +1,139 @@
 <?php
 
 $access_token = 'OQyobbkeIxjWwJQxsbGeXH/tUGgjeF92a1MwWk4CnQ8R8f5UOnf84SFiApseMJLsl9K4JumE/4wRNyjFmcbfR64jQIXfYaJzRNiqV968mzSpacYpGpdICwBx29tQiQmufK6dYF5wgQDhRTBIhel5GwdB04t89/1O/w1cDnyilFU=';
-
 // Get POST body content
-
 $content = file_get_contents('php://input');
-
 // Parse JSON
-
 $events = json_decode($content, true);
-
 // Validate parsed JSON data
-
-if (!is_null($events['events'])) {
-
-    // Loop through each event
-
-    foreach ($events['events'] as $event) {
-
-        // Reply only when message sent is in 'text' format
-
-        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-
-            // Get text sent
-
-            $text = $event['message']['text'];
-
-            // Get replyToken
-
-            $replyToken = $event['replyToken'];
-
-            // Build message to reply back
-
-            $messages = [
-
-                'type' => 'text',
-
-                'text' => $text,
-
-            ];
-
-            // Make a POST Request to Messaging API to reply to sender
-
-            $url = 'https://api.line.me/v2/bot/message/reply';
-
-            $data = [
-
-                'replyToken' => $replyToken,
-
-                'messages' => [$messages]
-
-            ];
-
-            $post = json_encode($data);
-
-            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-            $ch = curl_init($url);
-
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-
-            $result = curl_exec($ch);
-
-            curl_close($ch);
-
-            echo $result . "";
-
+$_msg = $events['events'][0]['message']['text'];
+$Pdata = array("CMI","CRI","LPG","LPN","MHS","NAN","PHE","PYO");
+$bMsg =substr ($_msg,-7,3);
+$SiteMsg =substr ($_msg,-7);
+$Sitedata = array("CMI0027","MHS0001");
+if (strpos($_msg,'-sitetech') !== false ){
+    for ($i = 0 ; i<8 ; $i++){
+        if ($bMsg == $Pdata[$i]){
+            for ($j = 0 ; $j<3539 ;$j++){
+                if ($SiteMsg == $Sitedata[$j]){
+                    // Get text sent
+                    $text = 'หาเองดิ tech อะ';
+                    // Get replyToken
+                    $replyToken = $events['events'][0]['replyToken'];
+                    // Build message to reply back
+                    $messages = [
+                        'type' => 'text',
+                        'text' => $text,
+                    ];
+                    // Make a POST Request to Messaging API to reply to sender
+                    $url = 'https://api.line.me/v2/bot/message/reply';
+                    $data = [
+                        'replyToken' => $replyToken,
+                        'messages' => [$messages]
+                    ];  
+                }
+            }
         }
-
     }
-
 }
 
+else if (strpos($_msg,'-siteaddr') !== false ){
+    for ($i = 0 ; i<8 ; $i++){
+        if ($bMsg == $Pdata[$i]){
+            for ($j = 0 ; $j<3539 ;$j++){
+                if ($SiteMsg == $Sitedata[$j]){
+                    // Get text sent
+                    $text = 'หาเองดิ addr อะ';
+                    // Get replyToken
+                    $replyToken = $events['events'][0]['replyToken'];
+                    // Build message to reply back
+                    $messages = [
+                        'type' => 'text',
+                        'text' => $text,
+                    ];
+                    // Make a POST Request to Messaging API to reply to sender
+                    $url = 'https://api.line.me/v2/bot/message/reply';
+                    $data = [
+                        'replyToken' => $replyToken,
+                        'messages' => [$messages]
+                    ]; 
+                }
+            }
+        }
+    }
+}
+else if (strpos($_msg,'-siteloc') !== false ){
+    for ($i = 0 ; i<8 ; $i++){
+        if ($bMsg == $Pdata[$i]){
+            for ($j = 0 ; $j<3539 ;$j++){
+                if ($SiteMsg == $Sitedata[$j]){
+                    // Get text sent
+                    $text = 'หาเองดิ loc อะ';
+                    // Get replyToken
+                    $replyToken = $events['events'][0]['replyToken'];
+                    // Build message to reply back
+                    $messages = [
+                        'type' => 'text',
+                        'text' => $text,
+                    ];
+                    // Make a POST Request to Messaging API to reply to sender
+                    $url = 'https://api.line.me/v2/bot/message/reply';
+                    $data = [
+                        'replyToken' => $replyToken,
+                        'messages' => [$messages]
+                    ]; 
+                }
+            }
+        }
+    }
+}
+else if (strpos($_msg,'-help') !== false ){
+    // Get text sent
+    $text = 'ไม่ช่วยได้ป่ะจะนอน';
+    // Get replyToken
+    $replyToken = $events['events'][0]['replyToken'];
+    // Build message to reply back
+    $messages = [
+        'type' => 'text',
+        'text' => $text,
+    ];
+    // Make a POST Request to Messaging API to reply to sender
+    $url = 'https://api.line.me/v2/bot/message/reply';
+    $data = [
+        'replyToken' => $replyToken,
+        'messages' => [$messages]
+    ];    
+}
+else{
+    // Get text sent
+    $text = 'ไม่พบข้อมูล '.$_msg;
+    // Get replyToken
+    $replyToken = $events['events'][0]['replyToken'];
+    // Build message to reply back
+    $messages = [
+        'type' => 'text',
+        'text' => $text,
+    ];
+    // Make a POST Request to Messaging API to reply to sender
+    $url = 'https://api.line.me/v2/bot/message/reply';
+    $data = [
+        'replyToken' => $replyToken,
+        'messages' => [$messages]
+    ];
+}
+
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "";
+
+
 echo "OK";
+?>
