74761b52232e05d1efc5a3059bebe32b65eef5d7
Piyasopon <nottee.p@hotmail.com>
2019-07-02 11:52:35 +0700

    test

--- readcsv.php
+++ readcsv.php
@@ -8,4 +8,5 @@ while (($objArr = fgetcsv($objCSV, 1000, ",")) !== FALSE) {
  }
 fclose($objCSV);
 echo "OK";
+echo $objArr[0]." ".$objArr[1]." ".$objArr[2]." ".$objArr[3]." ".$objArr[4]." ".$objArr[5]." ".$objArr[6]." ".$objArr[7]." ".$objArr[8]." ".$objArr[9]." ".$objArr[10]." ".$objArr[11]." ".$objArr[12]." ".$objArr[13]."</br>";
 ?>
