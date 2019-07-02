ad35ceb3d58c094bafe5f63e0474c8d526872e71
Piyasopon <nottee.p@hotmail.com>
2019-07-02 13:43:09 +0700

    test

--- readcsv.php
+++ readcsv.php
@@ -3,7 +3,7 @@ $row = 1;
 $objCSV = fopen("Site Data for TSID2.csv", "r");
 while (($objArr = fgetcsv($objCSV, 1000, ",")) !== FALSE) {
     $num = count($data);
-   if ($objArr[0]=='CMI'){echo $objArr[0]." ".$objArr[1]." ".$objArr[2]." ".$objArr[3]." ".$objArr[4]." ".$objArr[5]." ".$objArr[6]." ".$objArr[7]." ".$objArr[8]." ".$objArr[9]." ".$objArr[10]." ".$objArr[11]." ".$objArr[12]." ".$objArr[13]."</br>";}
+   echo $objArr[0]." ".$objArr[1]." ".$objArr[2]." ".$objArr[3]." ".$objArr[4]." ".$objArr[5]." ".$objArr[6]." ".$objArr[7]." ".$objArr[8]." ".$objArr[9]." ".$objArr[10]." ".$objArr[11]." ".$objArr[12]." ".$objArr[13]."</br>";
     $row++;
     
  }
