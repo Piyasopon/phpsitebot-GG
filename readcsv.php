70d3dc3b663a72f01a9d1b5ef15e7ef851de8de5
Piyasopon <nottee.p@hotmail.com>
2019-07-02 14:11:39 +0700

    test

--- readcsv.php
+++ readcsv.php
@@ -1,11 +1,12 @@
 <?php
 $row = 1;
-$objCSV = fopen("Site Data for TSID2.csv", "r");
+$i=0;
+$objCSV = fopen("Book1.csv", "r");
 while (($objArr = fgetcsv($objCSV, 1000, ",")) !== FALSE) {
     $num = count($data);
-   echo $objArr[0]." ".$objArr[1]." ".$objArr[2]." ".$objArr[3]." ".$objArr[4]." ".$objArr[5]." ".$objArr[6]." ".$objArr[7]." ".$objArr[8]." ".$objArr[9]." ".$objArr[10]." ".$objArr[11]." ".$objArr[12]." ".$objArr[13]."</br>";
+   if ($objArr[14]=="CMI"){echo $objArr[0];}
     $row++;
-    
+    $i++;if($i==50){'<br>';$i=0;}
  }
 
 echo "OK";
