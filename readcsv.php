a1ea1a44c0d87c751fc8aba38929913a6a065a5b
Piyasopon <nottee.p@hotmail.com>
2019-07-03 11:40:42 +0700

    8;p

--- readcsv.php
+++ readcsv.php
@@ -1,28 +1,27 @@
 <?php
-$CMIL = array(["CMI0027",	"เมืองเชียงใหม่",	"หายยา",	"18.78013",	"98.98756",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0033",	"เมืองเชียงใหม่",	"ช้างเผือก",	"18.81422",	"98.9825",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0034",	"เมืองเชียงใหม่",	"สุเทพ",	"18.78762",	"98.97255",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0036",	"เมืองเชียงใหม่",	"สุเทพ",	"18.79547",	"98.97758",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0039",	"เมืองเชียงใหม่",	"ศรีภูมิ",	"18.7894",	"98.9877",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0040",	"เมืองเชียงใหม่",	"แม่เหียะ",	"18.73444",	"98.95696",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0041",	"เมืองเชียงใหม่",	"ป่าแดด",	"18.76357",	"98.99277",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0042",	"เมืองเชียงใหม่",	"ช้างคลาน",	"18.77614",	"98.99774",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0043",	"เมืองเชียงใหม่",	"หายยา",	"18.77964",	"98.99375",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0044",	"เมืองเชียงใหม่",	"หนองหอย",	"18.76387",	"99.00655",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0046",	"เมืองเชียงใหม่",	"ช้างม่อย",	"18.7894",	"98.9968",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0047",	"เมืองเชียงใหม่",	"ช้างม่อย",	"18.79534",	"98.99497",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0048",	"เมืองเชียงใหม่",	"ช้างคลาน",	"18.78226",	"99.00189",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0049",	"เมืองเชียงใหม่",	"วัดเกต",	"18.78623",	"99.01476",	"Active",	"",	"Active",	"Active",	"Active",	"Active",]
-["CMI0050",	"เมืองเชียงใหม่",	"หนองป่าครั่ง",	"18.78538",	"99.02356",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0051",	"เมืองเชียงใหม่",	"ท่าศาลา",	"18.7684",	"99.02842",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
-["CMI0052",	"เมืองเชียงใหม่",	"หนองป่าครั่ง",	"18.79129",	"99.03021",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",]
+$CMIL = array("CMI0027",	"เมืองเชียงใหม่",	"หายยา",	"18.78013",	"98.98756",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0033",	"เมืองเชียงใหม่",	"ช้างเผือก",	"18.81422",	"98.9825",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0034",	"เมืองเชียงใหม่",	"สุเทพ",	"18.78762",	"98.97255",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0036",	"เมืองเชียงใหม่",	"สุเทพ",	"18.79547",	"98.97758",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0039",	"เมืองเชียงใหม่",	"ศรีภูมิ",	"18.7894",	"98.9877",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0040",	"เมืองเชียงใหม่",	"แม่เหียะ",	"18.73444",	"98.95696",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0041",	"เมืองเชียงใหม่",	"ป่าแดด",	"18.76357",	"98.99277",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0042",	"เมืองเชียงใหม่",	"ช้างคลาน",	"18.77614",	"98.99774",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0043",	"เมืองเชียงใหม่",	"หายยา",	"18.77964",	"98.99375",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0044",	"เมืองเชียงใหม่",	"หนองหอย",	"18.76387",	"99.00655",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0046",	"เมืองเชียงใหม่",	"ช้างม่อย",	"18.7894",	"98.9968",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0047",	"เมืองเชียงใหม่",	"ช้างม่อย",	"18.79534",	"98.99497",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0048",	"เมืองเชียงใหม่",	"ช้างคลาน",	"18.78226",	"99.00189",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0049",	"เมืองเชียงใหม่",	"วัดเกต",	"18.78623",	"99.01476",	"Active",	"",	"Active",	"Active",	"Active",	"Active",
+"CMI0050",	"เมืองเชียงใหม่",	"หนองป่าครั่ง",	"18.78538",	"99.02356",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0051",	"เมืองเชียงใหม่",	"ท่าศาลา",	"18.7684",	"99.02842",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
+"CMI0052",	"เมืองเชียงใหม่",	"หนองป่าครั่ง",	"18.79129",	"99.03021",	"Active",	"Active",	"Active",	"Active",	"Active",	"Active",
 );
 
 
-for ($i=0;$i<43;$i++){
-    for($j=0;$j<12;$j++){
-        echo $CMIL[$i][$j];
-    }
+for ($i=0;$i<100;$i++){
+        echo $CMIL[$i];
+    
 }
 echo "OK";
 ?>
