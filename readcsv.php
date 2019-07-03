b475ff30469c6a16c1583466934bfce0292cad37
Piyasopon <nottee.p@hotmail.com>
2019-07-03 16:40:01 +0700

    8;p

--- readcsv.php
+++ readcsv.php
@@ -1,7 +1,5 @@
 <?php
 include("crisite.php");
 $SiteMRF = "CMI6789";
-sreachcri();
-echo $Sitedata.$AMPHOE.$TAMBON.$LATITUDE.$LONGITUDE.$G900.$U850.$U2100.$L2100.$L1800.$L900;
 echo "OK readcsv";
 ?>
