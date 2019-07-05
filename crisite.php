a24a214ec61652c21850fe188f40eca66a797e7d
Piyasopon <nottee.p@hotmail.com>
2019-07-05 13:34:28 +0700

    test

--- crisite.php
+++ crisite.php
@@ -1,5 +1,22 @@
 <?php
-
+$accessToken = "";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+$content = file_get_contents('php://input');
+   $arrayJson = json_decode($content, true);
+$arrayHeader = array();
+   $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+//รับข้อความจากผู้ใช้
+   $message = $arrayJson['events'][0]['message']['text'];
+//รับ id ว่ามาจากไหน
+   if(isset($arrayJson['events'][0]['source']['userId']){
+      $id = $arrayJson['events'][0]['source']['userId'];
+   }
+   else if(isset($arrayJson['events'][0]['source']['groupId'])){
+      $id = $arrayJson['events'][0]['source']['groupId'];
+   }
+   else if(isset($arrayJson['events'][0]['source']['room'])){
+      $id = $arrayJson['events'][0]['source']['room'];
+   }
 
 
 echo "OK crisite";
