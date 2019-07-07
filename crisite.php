ca681253c38e62084f9a66a25b9f19796f356136
Piyasopon <nottee.p@hotmail.com>
2019-07-07 22:01:51 +0700

    test code

--- crisite.php
+++ crisite.php
@@ -1,17 +1,56 @@
 <?php
-$accessToken = "";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
-$content = file_get_contents('php://input');
-   $arrayJson = json_decode($content, true);
-$arrayHeader = array();
-   $arrayHeader[] = "Content-Type: application/json";
-   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
-//รับข้อความจากผู้ใช้
-   $message = $arrayJson['events'][0]['message']['text'];
-//รับ id ว่ามาจากไหน
-      $id1 = $arrayJson['events'][0]['source']['userId'];
-      $id2 = $arrayJson['events'][0]['source']['groupId'];
-      $id3 = $arrayJson['events'][0]['source']['room'];
+{
+   $access_token = 'QTT5Jps7jzDie8k/PmEXPPWEnwshJjuUgdnzOIxwM1Smbo5LpLLTcH6IH1Yak/kcpBRQ1Mg6zgbhRhJRfeiTrMQu00gKSjje90+BzC1R1XF4hmDOorADW/MQyGAMNkE7/MI/vDAy9juWhix1dMppqgdB04t89/1O/w1cDnyilFU=';
+   // Get POST body content
+   $content = file_get_contents('php://input');
+   $arrayHeader = array();
+      $arrayHeader[] = "Content-Type: application/json";
+      $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+   // Parse JSON
+   $events = json_decode($content, true);
+   $id1 = $events['events'][0]['source']['userId'];
+   $id2 = $events['events'][0]['source']['groupId'];
+   $id3 = $events['events'][0]['source']['room'];
+   // Validate parsed JSON data
+   $_msg = $events['events'][0]['message']['text'];
+   
 
+   $messages= [
+     {
+       "type"= "text",
+       "text"= "Hello Quick Reply!",
+       "quickReply"= {
+         "items"= [
+           {
+            "type"=> "action",
+             "action"=> {
+               "type"=> "camera",
+               "label"=> "Camera"
+             }
+           }
+         ]
+      }
+     }
+    ]
+ }
 
-echo "OK crisite";
+ $replyToken = $events['events'][0]['replyToken'];
+     $url = 'https://api.line.me/v2/bot/message/reply';
+     $data = [
+         'replyToken' => $replyToken,
+         'messages' => [$messages]
+     ];
+
+ $post = json_encode($data);
+ $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+ $ch = curl_init($url);
+ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+ $result = curl_exec($ch);
+ curl_close($ch);
+ echo $result . "";
+ echo "OK";
 ?>
