5de1824c8678ba04fb3078a6d3452b61ad9b0fe4
Piyasopon <nottee.p@hotmail.com>
2019-07-07 22:40:52 +0700

    test

--- crisite.php
+++ crisite.php
@@ -1,24 +1,33 @@
 <?php
 
-   $access_token = 'pyy3ejY2dZqlv1IzEQWlaVYa8/avCiMSl18URYL/aUTbPElz68myv31ssA/xVbePpBRQ1Mg6zgbhRhJRfeiTrMQu00gKSjje90+BzC1R1XEG5MVGZZmn7r0TyGySNLywhb9oOW6tbpCMVMdwfiu58QdB04t89/1O/w1cDnyilFU=';
+$access_token = 'pyy3ejY2dZqlv1IzEQWlaVYa8/avCiMSl18URYL/aUTbPElz68myv31ssA/xVbePpBRQ1Mg6zgbhRhJRfeiTrMQu00gKSjje90+BzC1R1XEG5MVGZZmn7r0TyGySNLywhb9oOW6tbpCMVMdwfiu58QdB04t89/1O/w1cDnyilFU=';
    // Get POST body content
-   $content = file_get_contents('php://input');
-   $arrayHeader = array();
-      $arrayHeader[] = "Content-Type: application/json";
-      $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+$content = file_get_contents('php://input');
+$arrayHeader = array();
+$arrayHeader[] = "Content-Type: application/json";
+$arrayHeader[] = "Authorization: Bearer {$accessToken}";
    // Parse JSON
-   $events = json_decode($content, true);
-   $id1 = $events['events'][0]['source']['userId'];
-   $id2 = $events['events'][0]['source']['groupId'];
-   $id3 = $events['events'][0]['source']['room'];
+$events = json_decode($content, true);
+$id1 = $events['events'][0]['source']['userId'];
+$id2 = $events['events'][0]['source']['groupId'];
+$id3 = $events['events'][0]['source']['room'];
    // Validate parsed JSON data
-   $_msg = $events['events'][0]['message']['text'];
+$_msg = $events['events'][0]['message']['text'];
    
-   $text = 'ฮั้นแน่ !';
-   $replyToken = $events['events'][0]['replyToken'];
-   $messages = [
+$text = 'ฮั้นแน่ !';
+$replyToken = $events['events'][0]['replyToken'];
+$messages = [
            'type' => 'text',
            'text' => $text,
+           "quickReply"= {
+            "items"= [
+              {
+                "type"=> "action",
+                "action"=> {
+                  "type"=> "cameraRoll",
+                  "label"=> "Camera Roll"
+                }
+              }]}
        ];
        $url = 'https://api.line.me/v2/bot/message/reply';
        $data = [
@@ -26,16 +35,16 @@
            'messages' => [$messages]
        ];
 
- $post = json_encode($data);
- $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
- $ch = curl_init($url);
- curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
- curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
- curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
- $result = curl_exec($ch);
- curl_close($ch);
- echo $result . "";
- echo "OK";
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "";
+echo "OK";
 ?>
