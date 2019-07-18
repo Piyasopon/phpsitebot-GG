8a3722bf5f0a3adf3396e01fcfcdc027f4081e8c
Piyasopon <nottee.p@hotmail.com>
2019-07-18 15:09:22 +0700

    test

--- readcsv.php
+++ readcsv.php
@@ -2,6 +2,6 @@
 include ('crisite.php');
 $myfile = fopen("SiteDataforTSID2.csv", "r") or die("Unable to open file!");
 echo "OK readcsv";
-$str = str_getcsv(fread($myfile,filesize("SiteDataforTSID2.csv")));
+$str = fread($myfile,filesize("SiteDataforTSID2.csv"));
 echo $str;
 ?>
