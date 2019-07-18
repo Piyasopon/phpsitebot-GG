266f840e449fb1f777b2499816dd9e1d02f4e189
Piyasopon <nottee.p@hotmail.com>
2019-07-18 17:12:05 +0700

    test

--- readcsv.php
+++ readcsv.php
@@ -4,6 +4,13 @@ $myfile = fopen("SiteDataforTSID2.csv", "r") or die("Unable to open file!");
 echo "OK readcsv";
 while(!feof($myfile)) {
 echo fgets($myfile);
-}
+}fclose($myfile);
+
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
 ?>
 
