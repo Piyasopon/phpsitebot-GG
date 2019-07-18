af21ea67962798c577d8767b443d2846db613106
Piyasopon <nottee.p@hotmail.com>
2019-07-18 14:43:14 +0700

    test

--- readcsv.php
+++ readcsv.php
@@ -3,7 +3,7 @@ include ('crisite.php');
 $myfile = fopen("SiteDataforTSID2.csv", "r") or die("Unable to open file!");
 ?>
 <script>
-var site = [<?php fread($myfile,filesize("SiteDataforTSID2.csv"));?>];
+var site = "<?php fread($myfile,filesize("SiteDataforTSID2.csv"));?>";
 </script>
 <?php
 echo "OK readcsv";
