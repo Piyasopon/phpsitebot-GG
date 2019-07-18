1e8004181fc216ff61f7aa6aaf4ca82ac6e971ee
Piyasopon <nottee.p@hotmail.com>
2019-07-18 16:46:27 +0700

    test

--- readcsv.php
+++ readcsv.php
@@ -2,10 +2,8 @@
 include ('crisite.php');
 $myfile = fopen("SiteDataforTSID2.csv", "r") or die("Unable to open file!");
 echo "OK readcsv";
-$str = fread($myfile,filesize("SiteDataforTSID2.csv"));
-$strexplode = explode(",",$str);
-while(!feof($myfile)) {
-  $str = fgets($myfile);
+while(($objArr = fgetcsv($myfile, 1000, ",")) !== FALSE)) {
+  $str = $objArr;
 }
 
 echo $str;
