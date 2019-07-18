2a7467fea1235f6deff424ecb3946202f04347d5
Piyasopon <nottee.p@hotmail.com>
2019-07-18 15:39:44 +0700

    test

--- readcsv.php
+++ readcsv.php
@@ -1,7 +1,5 @@
 <?php
-include ('crisite.php');
 $myfile = fopen("SiteDataforTSID2.csv", "r") or die("Unable to open file!");
-echo "OK readcsv";
 $str = fread($myfile,filesize("SiteDataforTSID2.csv"));
 $strexplode = explode(",",$str);
 $locations =[];
