e09244c4c3f50f224034a5b61fc59eebda19b345
Piyasopon <nottee.p@hotmail.com>
2019-07-22 14:53:44 +0700

    test

--- mongo.php
+++ mongo.php
@@ -1,7 +1,8 @@
 <?php
 $client = new MongoDB\Client('mongodb://admin:admin@cluster-shard-00-00-civtu.gcp.mongodb.net:27017,cluster-shard-00-01-civtu.gcp.mongodb.net:27017,cluster-shard-00-02-civtu.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster-shard-0&authSource=admin&retryWrites=true&w=majority');
-$db = $client->test;
-$collection = $db->selectCollection( "duckduck" );
+$connect = $client->test;
+$db = $connect->selectDB( "duckduck" );
+$collection = $db->selectCollection( "linebot" );
 $insert = array( "name" => "PHP","type" => "Computer Language");
 $collection->insert( $insert,true);//เพิ่มข้อมูล
 $showcate = $collection->find();//แสดงข้อมูลทั้งหมด
