d7e5dc72b14334c3be01a0eba457796de116ab9f
Piyasopon <nottee.p@hotmail.com>
2019-07-22 14:41:55 +0700

    test

--- readcsv.php
+++ readcsv.php
@@ -2,7 +2,7 @@
 include ('crisite.php');
 $myfile = fopen("SiteDataforTSID2.csv", "r") or die("Unable to open file!");
 echo "OK readcsv";
-while(!feof($myfile)) {
+while(!feof($myfile)){
 echo fgets($myfile);
 }fclose($myfile);
 
