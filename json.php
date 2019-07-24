9f3db2f0929ae1ff158336f60881d403af177895
Piyasopon <nottee.p@hotmail.com>
2019-07-24 17:18:50 +0700

    test

--- json.php
+++ json.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 $objConnect = mysql_connect("localhost","daboostu_tsid","tsidtsid2");
 $objDB = mysql_select_db("daboostu_tsid2");
 mysql_query("SET NAMES UTF8");
-$strSQL = "SELECT * FROM TABLE 7 ";
+$strSQL = "SELECT * FROM 'TABLE 7' ";
 $objQuery = mysql_query($strSQL);
 $resultArray = array();
 while($obResult = mysql_fetch_array($objQuery))
