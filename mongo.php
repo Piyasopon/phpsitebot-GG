1b9584b228b75c83f6fbfc9e2af3486906601446
Piyasopon <nottee.p@hotmail.com>
2019-07-24 13:47:57 +0700

    test

--- mongo.php
+++ mongo.php
@@ -1,5 +1,5 @@
 <?php
-$connect=new Mongo('mongodb+srv://admin:admin@cluster-civtu.gcp.mongodb.net/test?retryWrites=true&w=majority');//Default http://localhost:27017
+$connect=new Mongo('mongodb+srv:http//admin:admin@cluster-civtu.gcp.mongodb.net/test?retryWrites=true&w=majority');//Default http://localhost:27017
 $db = $connect->selectDB( "duckduck" );
 $collection = $db->selectCollection( "linebot" );
 ?>
