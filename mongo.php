39320195465526fbfc351b7c356a175350b32bc3
Piyasopon <nottee.p@hotmail.com>
2019-07-24 13:40:54 +0700

    test

--- mongo.php
+++ mongo.php
@@ -1,6 +1,16 @@
 <?php
+define('dbip', '127.0.0.1');
+define('dbport', '27017');
+define('dbuser', 'admin');
+define('dbpass', 'admin');
+$db = testdb;
+
+
+$connect = new MongoClient("mongodb://".$dbuser.":".$dbpass."@".$dbip.":".$dbport);
+$db = $connect->duckduck;
+$user = $db->user;
    // connect to mongodb
-   $m = new MongoClient('mongodb://admin:admin@@cluster-civtu.gcp.mongodb.net/test?retryWrites=true&w=majority');
+   $m = new Mongo\Client('mongodb://admin:admin@@cluster-civtu.gcp.mongodb.net/test?retryWrites=true&w=majority');
    echo "Connection to database successfully";
 	
    // select a database
@@ -20,3 +30,7 @@
    $collection->insert($document);
    echo "Document inserted successfully";
 ?>
+
+
+
+
