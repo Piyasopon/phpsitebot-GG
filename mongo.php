e3f4db07512f526d0d2028dde274077ffb80ef48
Piyasopon <nottee.p@hotmail.com>
2019-07-24 13:44:57 +0700

    test

--- mongo.php
+++ mongo.php
@@ -1,34 +1,7 @@
 <?php
-define('dbip', '127.0.0.1');
-define('dbport', '27017');
-define('dbuser', 'admin');
-define('dbpass', 'admin');
-$db = testdb;
-
-
-$connect = new MongoClient("mongodb://".$dbuser.":".$dbpass."@".$dbip.":".$dbport);
-$db = $connect->duckduck;
-$user = $db->user;
-   // connect to mongodb
-   $m = new Mongo\Client('mongodb://admin:admin@@cluster-civtu.gcp.mongodb.net/test?retryWrites=true&w=majority');
-   echo "Connection to database successfully";
-	
-   // select a database
-   $db = $m->duckduck;
-   echo "Database mydb selected";
-   $collection = $db->linebot;
-   echo "Collection selected succsessfully";
-	
-   $document = array( 
-      "title" => "MongoDB", 
-      "description" => "database", 
-      "likes" => 100,
-      "url" => "http://www.tutorialspoint.com/mongodb/",
-      "by" => "tutorials point"
-   );
-	
-   $collection->insert($document);
-   echo "Document inserted successfully";
+$connect=new Mongo('mongodb+srv://admin:admin@cluster-civtu.gcp.mongodb.net/test?retryWrites=true&w=majority');//Default http://localhost:27017
+$db = $connect->selectDB( "duckduck" );
+$collection = $db->selectCollection( "linebot" );
 ?>
 
 
