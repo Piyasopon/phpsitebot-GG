f42f8dbfe54f5fae8e227d55b9ce61afa04fcc47
Piyasopon <nottee.p@hotmail.com>
2019-07-25 10:24:53 +0700

    test

--- json.php
+++ json.php
@@ -4,8 +4,8 @@ $dbusername ="daboostu_tsid2";
 $dbpassword ="tsidtsid2";
 $dbname ="daboostu_tsid2";
 $con = new mysqli($host,$dbusername,$dbpassword,$dbname);
-$strSQL = "SELECT * FROM TABLE 7";
+$strSQL = "SELECT * FROM account";
 $objQuery = mysqli_query($con,$strSQL);
 $result = mysqli_fetch_assoc($objQuery);
-echo $result['COL 1'];
+echo $result['fullname'];
 ?>
