a3e869904e7c0d771cb61bd42afcbd6826021a04
Piyasopon <nottee.p@hotmail.com>
2019-07-25 11:57:46 +0700

    test

--- json.php
+++ json.php
@@ -5,7 +5,13 @@ $dbpassword ="tsidtsid2";
 $dbname ="daboostu_tsid2";
 $con = new mysqli($host,$dbusername,$dbpassword,$dbname);
 $strSQL = "SELECT * FROM TABLE 7";
-$objQuery = mysqli_query($con,$strSQL);
-$result = mysqli_fetch_assoc($objQuery);
-echo $result['COL 1'];
+$query = mysqli_query($con,$strSQL);
+$resultArray = array();
+while($result = mysqli_fetch_assoc($query) )
+{
+array_push($resultArray,$result);
+}
+
+mysqli_close($con);
+echo json_encode($resultArray);
 ?>
