04254e7941785e1ffb5cada57e7e12900423d05f
Piyasopon <nottee.p@hotmail.com>
2019-07-25 10:17:55 +0700

    test

--- json.php
+++ json.php
@@ -4,9 +4,6 @@ $dbusername ="daboostu_tsid";
 $dbpassword ="tsidtsid2";
 $dbname ="daboostu_tsid2";
 $con = new mysqli($host,$dbusername,$dbpassword,$dbname);
-
-header('Content-Type: application/json');
-mysql_query("SET NAMES UTF8");
 $strSQL = "SELECT * FROM TABLE 7";
 $objQuery = mysql_query($con,$strSQL);
 $result = mysqli_fetch_assoc($objQuery);
