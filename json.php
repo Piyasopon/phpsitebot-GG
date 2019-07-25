6e5c69b7876230e81b25cd3dbbd5018629d9988f
Piyasopon <nottee.p@hotmail.com>
2019-07-25 10:20:39 +0700

    test

--- json.php
+++ json.php
@@ -5,7 +5,7 @@ $dbpassword ="tsidtsid2";
 $dbname ="daboostu_tsid2";
 $con = new mysqli($host,$dbusername,$dbpassword,$dbname);
 $strSQL = "SELECT * FROM TABLE 7";
-$objQuery = mysql_query($con,$strSQL);
+$objQuery = mysqli_query($con,$strSQL);
 $result = mysqli_fetch_assoc($objQuery);
 echo $result['site'];
 ?>
