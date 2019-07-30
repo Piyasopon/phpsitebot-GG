a2823b1b536c2087f4b752deaae13415b67e54d0
Piyasopon <nottee.p@hotmail.com>
2019-07-30 12:08:39 +0700

    test

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+$client = new MongoDBClient('mongodb://admin:admin@@cluster-shard-00-00-civtu.gcp.mongodb.net:27017,cluster-shard-00-01-civtu.gcp.mongodb.net:27017,cluster-shard-00-02-civtu.gcp.mongodb.net:27017/duckduck?ssl=true&replicaSet=Cluster-shard-0&authSource=admin&retryWrites=true&w=majority');
+$db = $client->selectDB( "duckduck" );
+$collection = $db->selectCollection( "linebot" );
+
+?>
