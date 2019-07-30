2cc4470809424de1c206e49559ceb0a01530a33d
Piyasopon <nottee.p@hotmail.com>
2019-07-30 15:50:59 +0700

    test

--- mongo.php
+++ mongo.php
@@ -1,10 +1,15 @@
 
 <html>
 <body>
-
-<center><form action="action_page.php" method="post" style="font-size:40px">
-Search Site ::  <input type="text" name="site" value="CMI0034"><br><br>
-<input type="submit">
+<style>
+.input{
+    height:80px;
+    font-size:80px;
+}
+</style>
+<center><form action="action_page.php" method="post" style="font-size:80px">
+Search Site<br><input type="text" name="site" value="CMI0034" class="input"><br><br>
+<input type="submit" class="input" value="Search">
 </form></center>
 
 </body>
