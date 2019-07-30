4a665478b69eb4df542b53b3f06f9818d024e84b
Piyasopon <nottee.p@hotmail.com>
2019-07-30 16:35:40 +0700

    test

--- mongo.php
+++ mongo.php
@@ -9,13 +9,22 @@ body{
 .input{
     height:100px;
     font-size:60px;
+    font-family:"Times New Roman", Times, serif;
     border-radius: 5px;
 }
+.topnav {
+  overflow: hidden;
+  background-color: rgba(2, 2, 2, 0.7);
+  padding:20%;
+  margin:40px 10px 0px 10px ;
+  border-radius: 10px;
+}
 </style>
+<div class="topnav">
 <center><form action="action_page.php" method="post" style="font-size:80px">
 Search Site<br><input type="text" name="site" value="CMI0034" class="input"><br><br>
 <input type="submit" class="input" value="Search">
-</form></center>
+</form></center></div>
 
 </body>
 </html> 
