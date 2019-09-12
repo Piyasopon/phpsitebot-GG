35bb5982efce16771c7be2dd2d50b798b6decbf5
Piyasopon <nottee.p@hotmail.com>
2019-09-12 17:07:01 +0700

    test

---
+++ test.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <html>
+   
+      <script type = "text/javascript">
+         var watchID;
+         var geoLoc;
+         function showLocation(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            alert("Latitude : " + latitude + " Longitude: " + longitude);
+         }
+         function errorHandler(err) {
+            if(err.code == 1) {
+               alert("Error: Access is denied!");
+            } else if( err.code == 2) {
+               alert("Error: Position is unavailable!");
+            }
+         }
+         function getLocationUpdate(){
+            if(navigator.geolocation){
+               // timeout at 60000 milliseconds (60 seconds)
+               var options = {timeout:60000};
+               geoLoc = navigator.geolocation;
+               watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
+            } else {
+               alert("Sorry, browser does not support geolocation!");
+            }
+         }
+
+		 $(function(){
+    // โหลด สคริป google map api เมื่อเว็บโหลดเรียบร้อยแล้ว
+    // ค่าตัวแปร ที่ส่งไปในไฟล์ google map api
+    // v=3.2&sensor=false&language=th&callback=initialize
+    //  v เวอร์ชัน่ 3.2
+    //  sensor กำหนดให้สามารถแสดงตำแหน่งทำเปิดแผนที่อยู่ได้ เหมาะสำหรับมือถือ ปกติใช้ false
+    //  language ภาษา th ,en เป็นต้น
+    //  callback ให้เรียกใช้ฟังก์ชันแสดง แผนที่ initialize  
+    $("<script/>", {
+      "type": "text/javascript",
+      src: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCLqB76VXoO_24VlTEVTATn2qlEeKBR75k&language=th&v=3.exp&sensor=true&callback=initMap"
+    }).appendTo("body");    
+});
+      </script>
+   </head>
+   <body>
+   
+      <form>
+         <input type = "button" onclick = "getLocationUpdate();" value = "Watch Update"/>
+      </form>
+      
+   </body>
+</html>
