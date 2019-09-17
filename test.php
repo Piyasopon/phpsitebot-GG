5c24c0d091b95d207f4e12d0d4be02b957c39140
Piyasopon <52275237+Piyasopon@users.noreply.github.com>
2019-09-17 17:40:47 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,27 +1,60 @@
-<?php
-define('LINE_API','https://notify-api.line.me/api/notify');
- 
-$token = "YyRWnhMpMqMn5eDB1stOTAsQzJwBPr13kSJzDXkBSMs"; //ใส่Token ที่copy เอาไว้
-$str = "Hello"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
- 
-$res = notify_message($str,$token);
-print_r($res);
-function notify_message($message,$token){
- $queryData = array('message' => $message);
- $queryData = http_build_query($queryData,'','&');
- $headerOptions = array( 
-         'http'=>array(
-            'method'=>'POST',
-            'header'=> 'Content-Type: application/x-www-form-urlencoded\r\n'
-                      .'Authorization: Bearer '.$token.'\r\n'
-                      .'Content-Length: '.strlen($queryData).'\r\n',
-            'content' => $queryData
-         ),
- );
- $context = stream_context_create($headerOptions);
- $result = file_get_contents(LINE_API,FALSE,$context);
- $res = json_decode($result);
- return $res;
-}
+<!DOCTYPE HTML>
 
-?>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <html>
+   
+      <script type = "text/javascript">
+         var watchID;
+         var geoLoc;
+         function showLocation(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            alert("Latitude : " + latitude + " Longitude: " + longitude);
+         }
+         function errorHandler(err) {
+            if(err.code == 1) {
+               alert("Error: Access is denied!");
+            } else if( err.code == 2) {
+               alert("Error: Position is unavailable!");
+            }
+         }
+         function getLocationUpdate(){
+            if(navigator.geolocation){
+               // timeout at 60000 milliseconds (60 seconds)
+               var options = {timeout:60000};
+               geoLoc = navigator.geolocation;
+               watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
+            } else {
+               alert("Sorry, browser does not support geolocation!");
+            }
+         }
+
+		 $(function(){
+    // โหลด สคริป google map api เมื่อเว็บโหลดเรียบร้อยแล้ว
+    // ค่าตัวแปร ที่ส่งไปในไฟล์ google map api
+    // v=3.2&sensor=false&language=th&callback=initialize
+    //  v เวอร์ชัน่ 3.2
+    //  sensor กำหนดให้สามารถแสดงตำแหน่งทำเปิดแผนที่อยู่ได้ เหมาะสำหรับมือถือ ปกติใช้ false
+    //  language ภาษา th ,en เป็นต้น
+    //  callback ให้เรียกใช้ฟังก์ชันแสดง แผนที่ initialize  
+    $("<script/>", {
+      "type": "text/javascript",
+      src: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCLqB76VXoO_24VlTEVTATn2qlEeKBR75k&language=th&libraries=places&callback=initMap"
+    }).appendTo("body");    
+});
+      </script>
+   </head>
+   <body>
+   
+      <form>
+         <input type = "button" onclick = "getLocationUpdate();" value = "Watch Update"/>
+      </form>
+      
+   </body>
+</html>
