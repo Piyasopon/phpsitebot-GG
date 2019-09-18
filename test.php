f12e99398c0b5d7571eb5ecff600aee6d4e5b2ae
Piyasopon <52275237+Piyasopon@users.noreply.github.com>
2019-09-18 11:25:35 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,60 +1,46 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
+<html>
+<body>
 
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-   <html>
-   
-      <script type = "text/javascript">
-         var watchID;
-         var geoLoc;
-         function showLocation(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            alert("Latitude : " + latitude + " Longitude: " + longitude);
-         }
-         function errorHandler(err) {
-            if(err.code == 1) {
-               alert("Error: Access is denied!");
-            } else if( err.code == 2) {
-               alert("Error: Position is unavailable!");
-            }
-         }
-         function getLocationUpdate(){
-            if(navigator.geolocation){
-               // timeout at 60000 milliseconds (60 seconds)
-               var options = {timeout:60000};
-               geoLoc = navigator.geolocation;
-               watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
-            } else {
-               alert("Sorry, browser does not support geolocation!");
-            }
-         }
+<p>Click the button to get your coordinates.</p>
 
-		 $(function(){
-    // โหลด สคริป google map api เมื่อเว็บโหลดเรียบร้อยแล้ว
-    // ค่าตัวแปร ที่ส่งไปในไฟล์ google map api
-    // v=3.2&sensor=false&language=th&callback=initialize
-    //  v เวอร์ชัน่ 3.2
-    //  sensor กำหนดให้สามารถแสดงตำแหน่งทำเปิดแผนที่อยู่ได้ เหมาะสำหรับมือถือ ปกติใช้ false
-    //  language ภาษา th ,en เป็นต้น
-    //  callback ให้เรียกใช้ฟังก์ชันแสดง แผนที่ initialize  
-    $("<script/>", {
-      "type": "text/javascript",
-      src: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCLqB76VXoO_24VlTEVTATn2qlEeKBR75k&language=th&libraries=places&callback=initMap"
-    }).appendTo("body");    
-});
-      </script>
-   </head>
-   <body>
-   
-      <form>
-         <input type = "button" onclick = "getLocationUpdate();" value = "Watch Update"/>
-      </form>
-      
-   </body>
+<button onclick="getLocation()">Try It</button>
+
+<p id="demo"></p>
+
+<script>
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
+}
+</script>
+
+</body>
 </html>
