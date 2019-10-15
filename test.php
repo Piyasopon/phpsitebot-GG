b6d63e935d0030a9c1a9f68653fcc58536946d51
Piyasopon <nottee.p@hotmail.com>
2019-10-15 10:17:38 +0700

    test

--- test.php
+++ test.php
@@ -9,7 +9,12 @@
     for( $i = 0 ; $i < count($site) ; $i++ ){
       array_push($detail,explode( '","' , $site[$i] ));
     }
-  
+    for( $l = 0 ; $l < count($detail) ; $l++){
+      $detail[$l][0] = substr($detail[$l][0],-7);
+      $detail[$l][2] = substr($detail[$l][2],4);
+      $detail[$L][4] = substr($detail[$l][4],4);
+    }
+
   echo "<pre>";
   print_r($detail);
   echo "</pre>";
