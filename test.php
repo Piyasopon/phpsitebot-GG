019b8d43504986c96814633ece1237846f03411b
Piyasopon <nottee.p@hotmail.com>
2019-10-15 09:49:59 +0700

    test

--- test.php
+++ test.php
@@ -4,7 +4,15 @@
 
 <?php
   $map = file_get_contents('http://www.tsid2.daboostudio.com/sitebot/map_information.php');
-  echo $map[0][0];
+  $detail = [];
+  $site = explode('},{',$map);
+    for( $i = 0 ; $i < count($site) ; $i++ ){
+      array_push($detail,explode( '","' , $site[$i] ));
+    }
+  
+  echo "<pre>";
+  print_r($detail);
+  echo "</pre>";
 ?>
 
 </body>
