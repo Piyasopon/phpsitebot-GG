fd0a5ea3b2cece5df64e3973ca85784c52cdb9b4
Piyasopon <nottee.p@hotmail.com>
2019-10-16 10:23:07 +0700

    test

--- crisite.php
+++ crisite.php
@@ -63,43 +63,7 @@ if ($id2 == 'C58d56cb4045082304f1de057ad613d30' or $id1 == $ALUserID[$o]){
 
 
             $map = file_get_contents('http://www.tsid2.daboostudio.com/sitebot/test.php');
-            $site = str_replace('(','',str_replace('Array','',$map));
-            $_site = explode(')',$site);
-            $Arraysite = [];
 
-            for($i = 0 ; $i < count($_site) ; $i++){
-                if(strpos( $_site[$i] , $SiteMRF ) > 0){
-                $Site = str_replace('[0]','',$_site[$i]);
-                $Site = str_replace('[1]','',$Site);
-                $Site = str_replace('[2]','',$Site);
-                $Site = str_replace('[3]','',$Site);
-                $Site = str_replace('[4]','',$Site);
-                $Site = str_replace('[5]','',$Site);
-                $Site = str_replace('[6]','',$Site);
-                $Site = str_replace('[7]','',$Site);
-                $Site = str_replace('[8]','',$Site);
-                $Site = str_replace('[9]','',$Site);
-                $Site = str_replace('[10]','',$Site);
-                $Site = str_replace('[11]','',$Site);
-                $Site = str_replace('[12]','',$Site);
-                $Site = str_replace('[13]','',$Site);
-                $Site = str_replace('[14]','',$Site);
-                $Arraysite = explode('=>',$Site);
-                $PROVINCE= chop($Arraysite[4]);
-                $Sitedata= chop($Arraysite[3]);
-                            $AMPHOE=chop($Arraysite[5]);
-                            $TAMBON=chop($Arraysite[6]);
-                            $LATITUDE=chop($Arraysite[7]);
-                            $LONGITUDE=chop($Arraysite[8]);
-                            $G900=chop($Arraysite[10]);
-                            $U850=chop($Arraysite[11]);
-                            $U2100=chop($Arraysite[12]);
-                            $L2100=chop($Arraysite[13]);
-                            $L1800=chop($Arraysite[14]);
-                            $L900=chop($Arraysite[15]);
-                            break;    
-                }
-               }
          
 
             if (strpos($_msg,'-sitetech') !== false ){
