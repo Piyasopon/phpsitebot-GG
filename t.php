4fbe593aa842b3690d15b00b4920260255969f8b
Piyasopon <nottee.p@hotmail.com>
2019-11-07 10:22:17 +0700

    erw

--- t.php
+++ t.php
@@ -18,10 +18,46 @@ echo "OK";
 <script>
     function w3_FindnameNOP(){
         $.ajax({ 
-            url: "t2.php" ,
+            url: "t.php" ,
             type: "post",
             data: {"replyToken":<?php echo $replyToken ; ?>,"msg":<?php echo $_msg ; ?>}
         })
     }
     w3_FindnameNOP();
 </script>
+<?php
+
+$access_token = 'pyy3ejY2dZqlv1IzEQWlaVYa8/avCiMSl18URYL/aUTbPElz68myv31ssA/xVbePpBRQ1Mg6zgbhRhJRfeiTrMQu00gKSjje90+BzC1R1XEG5MVGZZmn7r0TyGySNLywhb9oOW6tbpCMVMdwfiu58QdB04t89/1O/w1cDnyilFU=';
+
+// Validate parsed JSON data
+$_msg = $_POST['msg'];
+
+
+
+$text = 'ฮั้นแน่ !';
+$replyToken = $_POST['replyToken'];
+$messages = [
+        'type' => 'text',
+        'text' => $text,
+    ];
+    $url = 'https://api.line.me/v2/bot/message/reply';
+    $data = [
+        'replyToken' => $replyToken,
+        'messages' => [$messages]
+    ];
+
+    $post = json_encode($data);
+    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    echo $result . "";
+    
+
+echo "OK";
+?>
